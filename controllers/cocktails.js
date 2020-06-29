15b0f4e0e43543d48af870914e6cd670f39b208c
ManliestBen <benjamintmanley@gmail.com>
2020-06-28 19:47:13 -0500

    add function

--- controllers/cocktails.js
+++ controllers/cocktails.js
@@ -1,5 +1,11 @@
 const Cocktail = require('../models/cocktail');
 
 module.exports = {
-    
+    create
+}
+
+function create(req, res) {
+    Cocktail.create(req.body)
+    .then(cocktail => {res.json(cocktail)})
+    .catch(err => {res.status(500).json(err)})
 }
--- routes/api.js
+++ routes/api.js
@@ -2,5 +2,6 @@ var express = require('express');
 var router = express.Router();
 const cocktailCtrl = require('../controllers/cocktails')
 
+router.post('/cocktails', cocktailCtrl.create);
 
 module.exports = router;
