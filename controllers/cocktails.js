0b14ef19e10b37016270585976205a7f844ccd07
ManliestBen <benjamintmanley@gmail.com>
2020-06-28 20:31:48 -0500

    complete code

--- controllers/cocktails.js
+++ controllers/cocktails.js
@@ -2,7 +2,10 @@ const Cocktail = require('../models/cocktail');
 
 module.exports = {
     create,
-    index
+    index,
+    show,
+    delete: deleteOne,
+    update
 }
 
 function create(req, res) {
@@ -14,5 +17,23 @@ function create(req, res) {
 function index(req, res) {
     Cocktail.find({})
     .then(cocktails => {res.json(cocktails)})
+    .catch(err => {res.status(500).json(err)})
+}
+
+function show(req, res) {
+    Cocktail.findById(req.params.id)
+    .then(cocktail => {res.json(cocktail)})
+    .catch(err => {res.status(500).json(err)})
+}
+
+function deleteOne(req, res) {
+    Cocktail.findByIdAndDelete(req.params.id)
+    .then(cocktail => {res.json(cocktail)})
+    .catch(err => {res.status(500).json(err)})
+}
+
+function update(req, res) {
+    Cocktail.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(cocktail => {res.json(cocktail)})
     .catch(err => {res.status(500).json(err)})
 }
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,8 @@ const cocktailCtrl = require('../controllers/cocktails')
 
 router.post('/cocktails', cocktailCtrl.create);
 router.get('/cocktails', cocktailCtrl.index);
-
+router.get('/cocktails/:id', cocktailCtrl.show);
+router.delete('/cocktails/:id', cocktailCtrl.delete);
+router.put('/cocktails/:id', cocktailCtrl.update);
 
 module.exports = router;
